5804e10b4023105d3956153d02669a8b7a7e236a
Jason Kridner <github@hangerhead.com>
2026-05-28 10:11:50 -0400

    Avoid GPIO reconfiguration on PCA9538 output writes

--- firmware/mspm0-gpo-extender/app/src/main.c
+++ firmware/mspm0-gpo-extender/app/src/main.c
@@ -4,108 +4,137 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/util.h>
 
-#define PCA9538_I2C_ADDR 0x20
-#define PCA9538_REG_INPUT 0x00
-#define PCA9538_REG_OUTPUT 0x01
-#define PCA9538_REG_POLARITY 0x02
-#define PCA9538_REG_CONFIG 0x03
-#define PCA9538_REG_COUNT 4
-#define PCA9538_GPIO_MASK 0x3f
-#define PCA9538_RESERVED_MASK 0xc0
+#define USER_NODE DT_PATH(zephyr_user)
+
+#define PCA9538_I2C_ADDR      0x20
+#define PCA9538_REG_INPUT     0x00
+#define PCA9538_REG_OUTPUT    0x01
+#define PCA9538_REG_POLARITY  0x02
+#define PCA9538_REG_CONFIG    0x03
+#define PCA9538_REG_MASK      0x03
+#define PCA9538_HW_GPIO_COUNT 6
+#define PCA9538_HW_MASK       GENMASK(PCA9538_HW_GPIO_COUNT - 1, 0)
 
 struct pca9538_state {
-	uint8_t regs[PCA9538_REG_COUNT];
+	uint8_t output;
+	uint8_t polarity;
+	uint8_t config;
 	uint8_t reg_ptr;
-	bool reg_ptr_set;
+	bool reg_ptr_valid;
 };
 
-static const struct gpio_dt_spec mux_gpios[] = {
-	GPIO_DT_SPEC_GET(DT_NODELABEL(mux_pins), rst_wrd_sel_gpios),
-	GPIO_DT_SPEC_GET(DT_NODELABEL(mux_pins), pwm_bit_sel_gpios),
-	GPIO_DT_SPEC_GET(DT_NODELABEL(mux_pins), an_di_sel_gpios),
-	GPIO_DT_SPEC_GET(DT_NODELABEL(mux_pins), int_do_sel_gpios),
-	GPIO_DT_SPEC_GET(DT_NODELABEL(mux_pins), cipo_cnt_sel0_gpios),
-	GPIO_DT_SPEC_GET(DT_NODELABEL(mux_pins), cipo_cnt_sel1_gpios),
+static const struct gpio_dt_spec mux_gpios[PCA9538_HW_GPIO_COUNT] = {
+	GPIO_DT_SPEC_GET(USER_NODE, rst_wrd_sel_gpios),
+	GPIO_DT_SPEC_GET(USER_NODE, pwm_bit_sel_gpios),
+	GPIO_DT_SPEC_GET(USER_NODE, an_di_sel_gpios),
+	GPIO_DT_SPEC_GET(USER_NODE, int_do_sel_gpios),
+	GPIO_DT_SPEC_GET(USER_NODE, cipo_cnt_sel0_gpios),
+	GPIO_DT_SPEC_GET(USER_NODE, cipo_cnt_sel1_gpios),
 };
 
-static const struct device *const i2c_dev = DEVICE_DT_GET(DT_NODELABEL(i2c0));
-static struct pca9538_state pca_state;
+static const struct device *const i2c_target = DEVICE_DT_GET(DT_NODELABEL(i2c0));
+static struct pca9538_state pca9538 = {
+	.output = 0xffU,
+	.polarity = 0x00U,
+	.config = 0xffU,
+	.reg_ptr = PCA9538_REG_INPUT,
+	.reg_ptr_valid = false,
+};
 
-static int pca9538_apply_gpio_config(const struct pca9538_state *state)
+static int pca9538_apply_output_latches(void)
 {
 	for (size_t i = 0; i < ARRAY_SIZE(mux_gpios); i++) {
-		uint8_t bit = BIT(i);
-		bool is_input = (state->regs[PCA9538_REG_CONFIG] & bit) != 0U;
-		int flags = is_input ? GPIO_INPUT : GPIO_OUTPUT_INACTIVE;
+		const struct gpio_dt_spec *spec = &mux_gpios[i];
+		bool is_input = (pca9538.config & BIT(i)) != 0U;
+		bool level = (pca9538.output & BIT(i)) != 0U;
+		int ret;
 
-		if (!device_is_ready(mux_gpios[i].port)) {
+		if (is_input) {
+			continue;
+		}
+
+		if (!gpio_is_ready_dt(spec)) {
 			return -ENODEV;
 		}
 
-		int ret = gpio_pin_configure_dt(&mux_gpios[i], flags);
-		if (ret != 0) {
+		ret = gpio_pin_set_dt(spec, level);
+		if (ret < 0) {
 			return ret;
-		}
-
-		if (!is_input) {
-			ret = gpio_pin_set_dt(&mux_gpios[i],
-						(state->regs[PCA9538_REG_OUTPUT] & bit) != 0U);
-			if (ret != 0) {
-				return ret;
-			}
 		}
 	}
 
 	return 0;
 }
 
-static uint8_t pca9538_read_input(const struct pca9538_state *state)
+static int pca9538_apply_config(void)
 {
-	uint8_t value = 0U;
-
 	for (size_t i = 0; i < ARRAY_SIZE(mux_gpios); i++) {
-		int ret = gpio_pin_get_dt(&mux_gpios[i]);
-		if (ret > 0) {
-			value |= BIT(i);
+		const struct gpio_dt_spec *spec = &mux_gpios[i];
+		bool is_input = (pca9538.config & BIT(i)) != 0U;
+		bool level = (pca9538.output & BIT(i)) != 0U;
+		gpio_flags_t flags =
+			is_input ? GPIO_INPUT : (level ? GPIO_OUTPUT_ACTIVE : GPIO_OUTPUT_INACTIVE);
+		int ret;
+
+		if (!gpio_is_ready_dt(spec)) {
+			return -ENODEV;
+		}
+
+		ret = gpio_pin_configure_dt(spec, flags);
+		if (ret < 0) {
+			return ret;
 		}
 	}
 
-	value ^= (state->regs[PCA9538_REG_POLARITY] & PCA9538_GPIO_MASK);
-	value &= PCA9538_GPIO_MASK;
-	return value;
+	return 0;
 }
 
-static uint8_t pca9538_get_register(const struct pca9538_state *state, uint8_t reg)
+static uint8_t pca9538_input_value(void)
 {
-	switch (reg) {
+	uint8_t value = pca9538.output & ~PCA9538_HW_MASK;
+
+	for (size_t i = 0; i < ARRAY_SIZE(mux_gpios); i++) {
+		int ret = gpio_pin_get_dt(&mux_gpios[i]);
+
+		if (ret > 0) {
+			value |= BIT(i);
+		} else {
+			value &= ~BIT(i);
+		}
+	}
+
+	return value ^ pca9538.polarity;
+}
+
+static uint8_t pca9538_read_reg(uint8_t reg)
+{
+	switch (reg & PCA9538_REG_MASK) {
 	case PCA9538_REG_INPUT:
-		return pca9538_read_input(state);
+		return pca9538_input_value();
 	case PCA9538_REG_OUTPUT:
+		return pca9538.output;
 	case PCA9538_REG_POLARITY:
+		return pca9538.polarity;
 	case PCA9538_REG_CONFIG:
-		return state->regs[reg];
+		return pca9538.config;
 	default:
-		return 0U;
+		return 0xffU;
 	}
 }
 
-static void pca9538_write_register(struct pca9538_state *state, uint8_t reg,
-				  uint8_t value)
+static void pca9538_write_reg(uint8_t reg, uint8_t value)
 {
-	switch (reg) {
+	switch (reg & PCA9538_REG_MASK) {
 	case PCA9538_REG_OUTPUT:
-		state->regs[PCA9538_REG_OUTPUT] &= ~PCA9538_GPIO_MASK;
-		state->regs[PCA9538_REG_OUTPUT] |= (value & PCA9538_GPIO_MASK);
-		(void)pca9538_apply_gpio_config(state);
+		pca9538.output = value;
+		(void)pca9538_apply_output_latches();
 		break;
 	case PCA9538_REG_POLARITY:
-		state->regs[PCA9538_REG_POLARITY] &= ~PCA9538_GPIO_MASK;
-		state->regs[PCA9538_REG_POLARITY] |= (value & PCA9538_GPIO_MASK);
+		pca9538.polarity = value;
 		break;
 	case PCA9538_REG_CONFIG:
-		state->regs[PCA9538_REG_CONFIG] = (value & PCA9538_GPIO_MASK) |
-					    PCA9538_RESERVED_MASK;
-		(void)pca9538_apply_gpio_config(state);
+		pca9538.config = value;
+		(void)pca9538_apply_config();
 		break;
 	default:
 		break;
@@ -115,44 +144,44 @@ static void pca9538_write_register(struct pca9538_state *state, uint8_t reg,
 static int pca9538_write_requested(struct i2c_target_config *cfg)
 {
 	ARG_UNUSED(cfg);
-	pca_state.reg_ptr_set = false;
+	pca9538.reg_ptr_valid = false;
 	return 0;
 }
 
-static int pca9538_write_received(struct i2c_target_config *cfg, uint8_t val)
+static int pca9538_write_received(struct i2c_target_config *cfg, uint8_t value)
 {
 	ARG_UNUSED(cfg);
 
-	if (!pca_state.reg_ptr_set) {
-		pca_state.reg_ptr = val % PCA9538_REG_COUNT;
-		pca_state.reg_ptr_set = true;
+	if (!pca9538.reg_ptr_valid) {
+		pca9538.reg_ptr = value & PCA9538_REG_MASK;
+		pca9538.reg_ptr_valid = true;
 		return 0;
 	}
 
-	pca9538_write_register(&pca_state, pca_state.reg_ptr, val);
-	pca_state.reg_ptr = (pca_state.reg_ptr + 1U) % PCA9538_REG_COUNT;
+	pca9538_write_reg(pca9538.reg_ptr, value);
+	pca9538.reg_ptr = (pca9538.reg_ptr + 1U) & PCA9538_REG_MASK;
 	return 0;
 }
 
-static int pca9538_read_requested(struct i2c_target_config *cfg, uint8_t *val)
+static int pca9538_read_requested(struct i2c_target_config *cfg, uint8_t *value)
 {
 	ARG_UNUSED(cfg);
 
-	if (!pca_state.reg_ptr_set) {
-		pca_state.reg_ptr = PCA9538_REG_INPUT;
-		pca_state.reg_ptr_set = true;
+	if (!pca9538.reg_ptr_valid) {
+		pca9538.reg_ptr = PCA9538_REG_INPUT;
+		pca9538.reg_ptr_valid = true;
 	}
 
-	*val = pca9538_get_register(&pca_state, pca_state.reg_ptr);
+	*value = pca9538_read_reg(pca9538.reg_ptr);
 	return 0;
 }
 
-static int pca9538_read_processed(struct i2c_target_config *cfg, uint8_t *val)
+static int pca9538_read_processed(struct i2c_target_config *cfg, uint8_t *value)
 {
 	ARG_UNUSED(cfg);
 
-	pca_state.reg_ptr = (pca_state.reg_ptr + 1U) % PCA9538_REG_COUNT;
-	*val = pca9538_get_register(&pca_state, pca_state.reg_ptr);
+	pca9538.reg_ptr = (pca9538.reg_ptr + 1U) & PCA9538_REG_MASK;
+	*value = pca9538_read_reg(pca9538.reg_ptr);
 	return 0;
 }
 
@@ -177,25 +206,18 @@ static struct i2c_target_config pca9538_target_cfg = {
 
 int main(void)
 {
-	if (!device_is_ready(i2c_dev)) {
+	if (!device_is_ready(i2c_target)) {
 		return 0;
 	}
 
-	pca_state.regs[PCA9538_REG_OUTPUT] = 0U;
-	pca_state.regs[PCA9538_REG_POLARITY] = 0U;
-	pca_state.regs[PCA9538_REG_CONFIG] = PCA9538_RESERVED_MASK;
-
-	if (pca9538_apply_gpio_config(&pca_state) != 0) {
+	if (pca9538_apply_config() < 0) {
 		return 0;
 	}
 
-	if (i2c_target_register(i2c_dev, &pca9538_target_cfg) != 0) {
+	if (i2c_target_register(i2c_target, &pca9538_target_cfg) < 0) {
 		return 0;
 	}
 
-	while (true) {
-		k_sleep(K_SECONDS(1));
-	}
-
+	k_sleep(K_FOREVER);
 	return 0;
 }
